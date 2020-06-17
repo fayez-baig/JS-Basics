ce96404439e012c431999a0cb51ea822cbba39a2
fayez baig <coderninja16@gmail.com>
2020-06-17 20:21:34 +0530

    replaced if with switch statements

--- control-02-monster-killer/assets/scripts/app.js
+++ control-02-monster-killer/assets/scripts/app.js
@@ -109,17 +109,36 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth,
   };
-  if ((ev = LOG_EVENT_PLAYER_ATTACK)) {
-    logEntry.target = "MONSTER";
-  } else if ((ev = LOG_EVENT_PLAYER_STRONG_ATTACK)) {
-    logEntry.target = "MONSTER";
-  } else if ((ev = LOG_EVENT_MONSTER_ATTACK)) {
-    logEntry.target = "PLAYER";
-  } else if ((ev = LOG_EVENT_PLAYER_HEAL)) {
-    logEntry.target = "PLAYER";
-  } else if ((ev = LOG_EVENT_GAME_OVER)) {
-    logEntry;
+  switch (ev) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = "PLAYER";
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = "PLAYER";
+      break;
+    case LOG_EVENT_GAME_OVER:
+      logEntry;
+      break;
+    default:
+      logEntry = {};
   }
+  // if ((ev = LOG_EVENT_PLAYER_ATTACK)) {
+  //   logEntry.target = "MONSTER";
+  // } else if ((ev = LOG_EVENT_PLAYER_STRONG_ATTACK)) {
+  //   logEntry.target = "MONSTER";
+  // } else if ((ev = LOG_EVENT_MONSTER_ATTACK)) {
+  //   logEntry.target = "PLAYER";
+  // } else if ((ev = LOG_EVENT_PLAYER_HEAL)) {
+  //   logEntry.target = "PLAYER";
+  // } else if ((ev = LOG_EVENT_GAME_OVER)) {
+  //   logEntry;
+  // }
   battleLog.push(logEntry);
 }
 
