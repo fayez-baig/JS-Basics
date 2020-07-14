645e02d8fe6dfff4576cc928697438429d7226a1
fayez baig <coderninja16@gmail.com>
2020-07-14 19:07:33 +0530

    ststic keyword

--- oop-06-getters-setters/assets/scripts/app.js
+++ oop-06-getters-setters/assets/scripts/app.js
@@ -17,7 +17,10 @@ class ShoppingCart {
 
   set cartItems(value) {
     this.items = value;
-    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
   }
 
   get totalAmount() {
@@ -35,13 +38,13 @@ class ShoppingCart {
   }
 
   render() {
-    const cartEl = document.createElement('section');
+    const cartEl = document.createElement("section");
     cartEl.innerHTML = `
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
-    cartEl.className = 'cart';
-    this.totalOutput = cartEl.querySelector('h2');
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -56,8 +59,8 @@ class ProductItem {
   }
 
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
     prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -69,8 +72,8 @@ class ProductItem {
           </div>
         </div>
       `;
-    const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
     return prodEl;
   }
 }
@@ -78,24 +81,24 @@ class ProductItem {
 class ProductList {
   products = [
     new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
+      "A Pillow",
+      "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      "A soft pillow!",
       19.99
     ),
     new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
+      "A Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      "A carpet which you might like - or not.",
       89.99
-    )
+    ),
   ];
 
   constructor() {}
 
   render() {
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
     for (const prod of this.products) {
       const productItem = new ProductItem(prod);
       const prodEl = productItem.render();
@@ -107,7 +110,7 @@ class ProductList {
 
 class Shop {
   render() {
-    const renderHook = document.getElementById('app');
+    const renderHook = document.getElementById("app");
 
     this.cart = new ShoppingCart();
     const cartEl = this.cart.render();
